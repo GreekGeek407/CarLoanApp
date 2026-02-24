8db41f620c0bde1dd68caf1a3d98364e51cbb0bb
android <android@gmail.com>
2026-02-24 12:18:45 -0500

    initial commit (unfinished)

---
+++ app/src/main/java/com/example/carloan/MainActivity.kt
@@ -0,0 +1,170 @@
+package com.example.carloan
+
+import android.os.Bundle
+import android.widget.RadioGroup
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.carloan.ui.theme.CarLoanTheme
+import java.text.Format
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CarLoanTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CarLoanScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CarLoanScreen(modifier: Modifier = Modifier) {
+    val promptSize = 20
+    val loanLengthOptions = listOf("36 months", "48 months", "60 months", "72 months", "84 months")
+    var purchasePrice by remember { mutableStateOf("") }
+    var downPayment by remember {mutableStateOf("")}
+    var interestRate by remember { mutableFloatStateOf(0f) }
+    var loanSize by remember { mutableIntStateOf(0) }
+    var monthlyPayment by remember { mutableDoubleStateOf(0.0) }
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Car Loan Calculator",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        TextFieldRow("Purchase Price:", promptSize, purchasePrice, {purchasePrice=it})
+        TextFieldRow("Down Payment Amount: ", promptSize, downPayment, {downPayment=it})
+        Row(
+            modifier = Modifier
+        ){
+            Text(
+                text = "Annual Interest Rate: " + String.format("%.1f", interestRate) + "%",
+                fontSize = promptSize.sp,
+                modifier = Modifier.padding(10.dp)
+            )
+            Slider(
+                value = interestRate,
+                onValueChange = {interestRate = it},
+                valueRange = 0f..100f,
+                modifier = Modifier.padding(horizontal = 10.dp)
+            )
+        }
+        RadioGroup(loanLengthOptions, promptSize)
+        Button(
+            onClick = {}
+        ){
+            Text("Calculate Monthly Payment")
+        }
+    }
+}
+
+@Composable
+fun TextFieldRow(prompt: String, size: Int, moneyValue: String, onChange: (String) -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            text = prompt,
+            fontSize = size.sp,
+            modifier = Modifier.padding(5.dp)
+        )
+        TextField(
+            value = moneyValue,
+            onValueChange = onChange,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            singleLine = true,
+            modifier = Modifier.padding(5.dp)
+        )
+    }
+}
+
+@Composable
+fun RadioGroup(
+    radioOptions: List<String>,
+    size: Int
+){
+    var selectedOption by remember {mutableStateOf("5 years")}
+    Column(
+
+    ) {
+        Text(
+            text = "Length of Loan:",
+            fontSize = size.sp,
+            modifier = Modifier.padding(5.dp)
+        )
+        radioOptions.forEach { option ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.selectable(
+                    selected = selectedOption == option,
+                    onClick = {selectedOption = option},
+                    role = Role.RadioButton
+                )
+            ){
+                RadioButton(
+                    selected = selectedOption == option,
+                    onClick = { selectedOption = option}
+                )
+                Text(
+                    text = option,
+                    fontSize = (size * 0.8).sp
+                )
+            }
+        }
+
+    }
+}
+
+fun calculateMonthlyPayment(
+    purchasePrice: Float,
+    downPayment: Float,
+    apr: Float,
+    loanLength: Int
+): Double{
+    val mr = apr/12
+    val loanSize = purchasePrice - downPayment
+    return mr * loanSize / (1-Math.pow((1+mr).toDouble(), ((loanLength * -1).toDouble())))
+}
